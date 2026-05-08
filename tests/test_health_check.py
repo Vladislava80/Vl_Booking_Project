938b8002d8881598231c246b28b6e27758e91e01
Владислава <olav80@internet.ru>
2026-05-08 21:21:28 +0300

    deleted "status" in "expected ..., but got..."

--- tests/test_health_check.py
+++ tests/test_health_check.py
@@ -24,7 +24,7 @@ def test_ping_wrong_method(api_client, mocker):
     mock_response = mocker.Mock()
     mock_response.status_code = 405
     mocker.patch.object(api_client.session, 'get', return_value=mock_response)
-    with pytest.raises(AssertionError, match='Expected status 201, but got 405'):
+    with pytest.raises(AssertionError, match='Expected 201, but got 405'):
         api_client.ping()
 
 
@@ -34,7 +34,7 @@ def test_ping_internal_server_error(api_client, mocker):
     mock_response = mocker.Mock()
     mock_response.status_code = 500
     mocker.patch.object(api_client.session, 'get', return_value=mock_response)
-    with pytest.raises(AssertionError, match='Expected status 201, but got 500'):
+    with pytest.raises(AssertionError, match='Expected 201, but got 500'):
         api_client.ping()
 
 
@@ -44,7 +44,7 @@ def test_ping_not_found(api_client, mocker):
     mock_response = mocker.Mock()
     mock_response.status_code = 404
     mocker.patch.object(api_client.session, 'get', return_value=mock_response)
-    with pytest.raises(AssertionError, match='Expected status 201, but got 404'):
+    with pytest.raises(AssertionError, match='Expected 201, but got 404'):
         api_client.ping()
 
 
@@ -54,7 +54,7 @@ def test_ping_success_different_code(api_client, mocker):
     mock_response = mocker.Mock()
     mock_response.status_code = 200
     mocker.patch.object(api_client.session, 'get', return_value=mock_response)
-    with pytest.raises(AssertionError, match='Expected status 201, but got 200'):
+    with pytest.raises(AssertionError, match='Expected 201, but got 200'):
         api_client.ping()
 
 
